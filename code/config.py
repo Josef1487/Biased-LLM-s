b8e01e06c1f342ac69b44609a28397ff0a695f27
Nic764fib <A7728620@gmail.com>
2026-01-21 12:42:32 +0100

    ..

--- code/config.py
+++ code/config.py
@@ -2,13 +2,13 @@
 # Hugging Face
 HF_MODELS = [
     # "TinyLlama/TinyLlama-1.1B-Chat-v1.0",        #first test
-    # "meta-llama/Meta-Llama-3-8B-Instruct", #token needed but owned is larger 8 billion
+     "meta-llama/Meta-Llama-3-8B-Instruct", #token needed but owned is larger 8 billion
     # "meta-llama/Llama-3.2-3B-Instruct",  #token needed but owned smaller
     # "mistralai/Mistral-7B-Instruct-v0.2",
     # "mistralai/Mistral-7B-Instruct-v0.1"
      #"microsoft/Phi-3.5-mini-instruct",
      #"microsoft/phi-2"
-     "Qwen/Qwen2.5-3B-Instruct",
+     #"Qwen/Qwen2.5-3B-Instruct",
      #"Qwen/Qwen1.5-4B-Chat"
 ]
 
